b5414f4a13757bac21ae6d5b77d10e901d007037
yumiko524 <k.yumiko19940524@gmail.com>
2020-06-22 09:58:14 +0900

    新しくページを追加
    CountとScore

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,22 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/count',
+    name: 'Count',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Count.vue')
+  },
+  {
+    path: '/score',
+    name: 'Score',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Score.vue')
   }
 ]
 
